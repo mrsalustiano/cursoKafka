1ca8b852efeafbe4b7765b2c9a51a5d6a9ef3a65
Marcelo S Silva <mrsalustiano@gmail.com>
2022-11-01 00:20:51 -0300

    Custom Consumer e Interceptor

--- str-consumer/src/main/java/br/mrsalustiano/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/br/mrsalustiano/strconsumer/listeners/StrConsumerListener.java
@@ -1,20 +1,31 @@
 package br.mrsalustiano.strconsumer.listeners;
 
+import br.mrsalustiano.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener( groupId = "group-1")
     public void create(String message) {
        log.info("Create  ::: Receive Message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener( groupId = "group-1")
     public void log(String message) {
         log.info("LOG ::: Receive Message {}", message);
     }
+
+
+    @KafkaListener( groupId = "group-2" , topics = "str-topic" , containerFactory = "validMessageContainerFactory" )
+    public void history(String message) {
+        log.info("HISTORY ::: Receive Message {}", message);
+    }
+
+
+
 }
