9604488e169bb945207b7dff9fdafdc45af43f35
Marcelo S Silva <mrsalustiano@gmail.com>
2022-11-26 03:35:13 -0300

    Finalizando Consumer

---
+++ Payment/json-consumer/src/test/java/br/mrsalustiano/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mrsalustiano.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
