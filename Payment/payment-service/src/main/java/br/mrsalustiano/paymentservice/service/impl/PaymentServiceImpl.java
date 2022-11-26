6421883f1de86f23d21c6054379517c0e6ee3912
Marcelo S Silva <mrsalustiano@gmail.com>
2022-11-26 02:55:49 -0300

    Add Producer Config

--- Payment/payment-service/src/main/java/br/mrsalustiano/paymentservice/service/impl/PaymentServiceImpl.java
+++ Payment/payment-service/src/main/java/br/mrsalustiano/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,17 +2,30 @@ package br.mrsalustiano.paymentservice.service.impl;
 
 import br.mrsalustiano.paymentservice.model.Payment;
 import br.mrsalustiano.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
         log.info("PAYMENT SERVICE IMPL ::: Recebido {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento");
+        kafkaTemplate.send("payment-topic", payment);
+
 
     }
 }
